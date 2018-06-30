862ce2ed3c8a1236911009f11db42ee5b34c554d
Grant Lamb <grantwlamb@gmail.com>
2018-06-29 17:40:46 -0700

    test

--- Chapter 3/Projects/Product.c
+++ Chapter 3/Projects/Product.c
@@ -1,11 +1,11 @@
-/*Buns not hot*/
+/*test*/
 #include <stdio.h>
 
 int main(void)
 {
 	int item, month, day, year;
 	float cost;
-	
+
 	/*Collects the information from the user */
 	printf("Enter item number: ");
 	scanf("%d", &item);
@@ -13,9 +13,9 @@ int main(void)
 	scanf("%f", &cost);
 	printf("Enter the purchase date (mm/dd/yyyy): ");
 	scanf("%d/%d/%d", &month, &day, &year);
-	
+
 	/*Prints out the inputted data into a special format*/
 	printf("Item\t\tUnit\t\tPurchase\n\t\tPrice\t\tDate\n%d\t\t$%7.2f\t%.2d/%.2d/%d ", item, cost, month, day, year);
-	
+
 	return 0;
 }
