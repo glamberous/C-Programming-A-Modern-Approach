bdbdbe8c1d02d20e9cc836d15cf7a23ef3fa52ad
Grant Lamb <grantwlamb@gmail.com>
2018-07-01 09:01:35 -0700

    Exercise 1 finished
    Which lines print new line properly?

--- Chapter 13/Exercise1.c
+++ Chapter 13/Exercise1.c
@@ -1,39 +1,41 @@
-/* Sorts an array of integers using Quicksort algorithm */
-/* Also Testing Github */
+/* Which lines print a new line properly */
+
 #include <stdio.h>		// for typef & scanf
 #include <ctype.h>		// for toupper
 
-#define N 5
-
-void max_min(int a[], int n, int *max, int *min);
 
 int main (void)
 {
-	int b[N], *p, big = 0, small = 0;
+	printf("%c", '\n');
+	printf("1. New Line? Y; %%c calls a character and ' ' signifies a character. \n"); // Answer N, "multi-character constant"
 	
-	printf("Enter %d numbers: ", N);
-	for (p = b; p < b+N; p++)
-		scanf("%d", p);
+	//printf("%c", "\n");
+	//printf("New Line? N; %%c calls a character and \" \" signifies a string. \n"); // Answer N, was expecting a pointer
 	
-	max_min(b, N, &big, &small);
+	//printf("%s", '\n');
+	//printf("New Line? N; %%s calls a string and ' ' signifies a character. \n"); // Answer N, Looking for a Char pointer
 	
-	printf("Largest: %d\n", big);
-	printf("Smallest: %d\n", small);
+	printf("%s", "\n");
+	printf("2. New Line? Y; %%s calls a string and \" \" signifies a string. \n");
 	
-	return 0;
-}
-
-void max_min(int a[], int n, int *max, int *min)
-{
-	int *p = a;
+	//printf('\n');
+	//printf("New Line? N; printf calls a string and ' ' signifies a character. \n"); // Answer N, is looking for a char pointer
 	
-	*max = *min = *p;
+	printf("\n");
+	printf("3. New Line? Y; printf calls a string and \" \" signifies a string. \n");
 	
-	for (p = a+1; p < a+n; p++)
-	{
-		if (*p > *max)
-			*max = *p;
-		else if (*p < *min)
-			*min = *p;
-	}
+	putchar('\n');
+	printf("4. New Line? Y; putchar calls a charater and ' ' signifies a character. \n"); // Answer N, "multi-character constant"
+	
+	//putchar("\n");
+	//printf("New Line? N; putchar calls a charater and " " signifies a string. \n"); // Answer N, Was expecting a char
+	
+	//puts('\n');
+	//printf("New Line? N; puts calls a string and ' ' signifies a character. \n"); // Answer N, is looking for a char pointer
+	
+	puts("\n");
+	printf("5. New Line? Y; puts calls a string and " " signifies a string. \n"); // Answer Y, but puts automatically puts a \n afterwards so this is two \n's
+	
+	puts("");
+	printf("6. New Line? N; \\0 doesn't represent \\n\n"); // Answer Y, automatically puts \n at the end	
 }
